561ad353d07711991bfaeb72a1bb27e7d37ef8e9
jhonse <admin@jhonse.com>
2017-01-02 15:35:05 +0800

    基本功能配置

--- jBatchPhone/jBatchPhone/Main.Designer.cs
+++ jBatchPhone/jBatchPhone/Main.Designer.cs
@@ -28,20 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tpBasic = new System.Windows.Forms.TabPage();
+            this.tpLog = new System.Windows.Forms.TabPage();
+            this.tpSimulator = new System.Windows.Forms.TabPage();
+            this.tpTask = new System.Windows.Forms.TabPage();
+            this.tcMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.tpBasic);
+            this.tcMain.Controls.Add(this.tpLog);
+            this.tcMain.Controls.Add(this.tpSimulator);
+            this.tcMain.Controls.Add(this.tpTask);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 0);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(664, 641);
+            this.tcMain.TabIndex = 0;
+            // 
+            // tpBasic
+            // 
+            this.tpBasic.Location = new System.Drawing.Point(4, 22);
+            this.tpBasic.Name = "tpBasic";
+            this.tpBasic.Padding = new System.Windows.Forms.Padding(3);
+            this.tpBasic.Size = new System.Drawing.Size(656, 615);
+            this.tpBasic.TabIndex = 0;
+            this.tpBasic.Text = "基本配置";
+            this.tpBasic.UseVisualStyleBackColor = true;
+            // 
+            // tpLog
+            // 
+            this.tpLog.Location = new System.Drawing.Point(4, 22);
+            this.tpLog.Name = "tpLog";
+            this.tpLog.Padding = new System.Windows.Forms.Padding(3);
+            this.tpLog.Size = new System.Drawing.Size(656, 615);
+            this.tpLog.TabIndex = 1;
+            this.tpLog.Text = "日志列表";
+            this.tpLog.UseVisualStyleBackColor = true;
+            // 
+            // tpSimulator
+            // 
+            this.tpSimulator.Location = new System.Drawing.Point(4, 22);
+            this.tpSimulator.Name = "tpSimulator";
+            this.tpSimulator.Size = new System.Drawing.Size(656, 615);
+            this.tpSimulator.TabIndex = 2;
+            this.tpSimulator.Text = "模拟器列表";
+            this.tpSimulator.UseVisualStyleBackColor = true;
+            // 
+            // tpTask
+            // 
+            this.tpTask.Location = new System.Drawing.Point(4, 22);
+            this.tpTask.Name = "tpTask";
+            this.tpTask.Size = new System.Drawing.Size(656, 615);
+            this.tpTask.TabIndex = 3;
+            this.tpTask.Text = "任务列表";
+            this.tpTask.UseVisualStyleBackColor = true;
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 561);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(664, 641);
+            this.Controls.Add(this.tcMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "jBatchPhone By Jhonse @2017-01-02 Version 1.0";
+            this.tcMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tpBasic;
+        private System.Windows.Forms.TabPage tpLog;
+        private System.Windows.Forms.TabPage tpSimulator;
+        private System.Windows.Forms.TabPage tpTask;
     }
 }
 
